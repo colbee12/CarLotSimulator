2c0f13c6ad4bdba4052de9ae28ac927d9c49747f
Colbee Thrasher <colbeethr@gmail.com>
2025-12-03 19:03:34 -0600

    made cars and parking lot

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace CarLotSimulator;
+
+public class Car
+{
+    //Create a seperate class file called Car
+    //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+    //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+    //The methods should take one string parameter: the respective noise property
+    
+    public int Year { get; set; }
+    public string Model { get; set; }
+    public string Make {get; set;}
+    public string EngineNoise { get; set; }
+    public string HonkNoise {get; set;}
+    public bool IsDriveable { get; set; }
+
+
+    public void MakeEnginNoise(string engineNoise)
+    {
+        EngineNoise = engineNoise;
+        Console.WriteLine($"{Make} {Model} has an engin noise that sounds like {engineNoise}");
+    }
+
+    public void MakeHonkNoise(string honkNoise)
+    {
+        HonkNoise = honkNoise;
+        Console.WriteLine($"{Make} {Model} has an engin noise that sounds like {HonkNoise}");
+    }
+
+
+    public Car()
+    {
+        
+    }
+
+
+
+    public Car(string make, string modle, int year, bool isDriveable)
+    {
+        Make = make;
+        Model = modle;
+        Year = year;
+        IsDriveable = isDriveable;
+    }
+}
+
+
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace CarLotSimulator
 {
@@ -6,14 +7,10 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var carLotOne = new CarLot();
+            
+            
             //TODO
-
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
-
-
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
@@ -22,12 +19,50 @@ namespace CarLotSimulator
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
+            var carOne = new Car();
+            carOne.Make = "Honda";
+            carOne.Model = "fit";
+            carOne.Year = 2000;
+            carOne.IsDriveable = true;
+            
+            carOne.MakeHonkNoise("honk");
+            carOne.MakeEnginNoise("vroom");
+
+            carLotOne.ParkingLot.Add(carOne);
+            
+            ///////////////////////////////
+
+            var carTwo = new Car()
+            {
+                Make = "BMW",
+                Model = "IXL",
+                Year = 2001,
+                IsDriveable = true,
+            };
+            carTwo.MakeHonkNoise("Beep");
+            carTwo.MakeEnginNoise("vroom Vroom");
+            
+            carLotOne.ParkingLot.Add(carTwo);
+            
+            
+            
+            ////////////////////////////////////
+
+            var carThree = new Car("Ford", "stang", 2006, true);
+            
+            carThree.MakeHonkNoise("honk Honk");
+            carThree.MakeEnginNoise("Roar");
+            
+            carLotOne.ParkingLot.Add(carThree);
+            
             //*************BONUS X 2*************//
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            
+            carLotOne.CheckCars();
         }
     }
 }
